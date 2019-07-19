a11ce9a70858c67108efbbf89dd50dd5ac8df5bb
Yuvraj Pawar <31062473+blacktig3r@users.noreply.github.com>
2019-07-19 10:41:08 +0530

    Create variables.tf
    taking variables from tfvars  file

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {}
+variable "aws_profile" {}
+
+
+#variable "elb_name" {}
+variable "elb_az" {}
+variable "elb_sg_group" {}
+variable "asg_name" {}
+#variable "aws_az" {}
+
+variable "tags_name" {}
+#variable "tags_insttype" {}
+
+variable "iam_role" {}
+variable "key_name" {}
+variable "ami_id" {}
+
